9dd6099ce68e09ebf5926ff1199e4c7e54dc3c4b
Lucas Vieira <vieira07lucas@gmail.com>
2023-01-25 16:07:32 -0300

    Implementando o botão jogar novamente e finalizando o projeto

--- app/validacao.js
+++ app/validacao.js
@@ -18,16 +18,21 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     document.body.innerHTML = `
             <h2>Parabéns, você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `;
+
   } else if (numero > numeroSecreto) {
     elementoChute.innerHTML += `
         <div>O número secreto é menor <i class="fa-solid fa-arrows-down-to-line"></i></div>
         `;
+
   } else {
     elementoChute.innerHTML += `
         <div>O número secreto é maior <i class="fa-solid fa-arrows-up-to-line"></i></div>
         `;
   }
+
 }
 
 function chuteForInvalido(numero) {
@@ -37,3 +42,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
   return numero > maiorValor || numero < menorValor;
 }
+
+document.body.addEventListener('click', e  => {
+    if (e.target.id === 'jogar-novamente') {
+        window.location.reload()
+    }
+})
