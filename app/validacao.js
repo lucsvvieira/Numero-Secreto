8f807ed359b137d5041cb231fbf6417af90e0c99
Lucas Vieira <vieira07lucas@gmail.com>
2023-01-25 15:54:28 -0300

    Criando dicas para o usuário poder acertar o número secreto

--- app/validacao.js
+++ app/validacao.js
@@ -1,30 +1,39 @@
 function verificaSeOChutePossuiUmValorValido(chute) {
-    const numero = +chute
+  const numero = +chute;
 
-    if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += `<div>Valor Inválido</div>`
-    }
+  if (chuteForInvalido(numero)) {
+    elementoChute.innerHTML += `<div>Valor Inválido</div>`;
+    return;
+  }
 
-    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-        elementoChute.innerHTML += `
+  if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+    elementoChute.innerHTML += `
         <div>Valor inválido: O número secreto precisa estar entre ${menorValor} e ${maiorValor}</div>
-        `
-    }
+        `;
 
+    return;
+  }
 
-    if (numero === numeroSecreto) {
-        document.body.innerHTML = `
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
             <h2>Parabéns, você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
-        `
-    }
-
+        `;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrows-down-to-line"></i></div>
+        `;
+  } else {
+    elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrows-up-to-line"></i></div>
+        `;
+  }
 }
 
 function chuteForInvalido(numero) {
-    return Number.isNaN(numero)
+  return Number.isNaN(numero);
 }
 
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
-    return numero > maiorValor || numero < menorValor
+  return numero > maiorValor || numero < menorValor;
 }
